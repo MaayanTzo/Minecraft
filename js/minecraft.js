2cbd164d501eb2a74bcc920f61c8048aea501803
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-12 16:20:46 +0200

    treat and leaves added

--- js/minecraft.js
+++ js/minecraft.js
@@ -58,43 +58,70 @@ function createTiles(array) {
 
 createTiles(matrixBackground);
 
+//change selected div classes (from the matrix) to create the cloud:
+
 function createCloud() {
     for (var i = 217; i < 219; i++) {
-        var cloud = $("div.tile:nth-child(" + i + "");
+        var cloud = $("div.tile:nth-child(" + i + ")");
         cloud.addClass("cloud");
     }
     for (var i = 245; i < 250; i++) {
-        var cloud = $("div.tile:nth-child(" + i + "");
+        var cloud = $("div.tile:nth-child(" + i + ")");
         cloud.addClass("cloud");
     }
     for (var i = 252; i < 255; i++) {
-        var cloud = $("div.tile:nth-child(" + i + "");
+        var cloud = $("div.tile:nth-child(" + i + ")");
         cloud.addClass("cloud");
     }
     for (var i = 274; i < 285; i++) {
-        var cloud = $("div.tile:nth-child(" + i + "");
+        var cloud = $("div.tile:nth-child(" + i + ")");
         cloud.addClass("cloud");
     }
     for (var i = 309; i < 312; i++) {
-        var cloud = $("div.tile:nth-child(" + i + "");
+        var cloud = $("div.tile:nth-child(" + i + ")");
         cloud.addClass("cloud");
     }
 }
 
 createCloud();
 
-/*
-function createCloud(array){
-    for (var i=0; i<array.length; i++){
-        for (var j=0; j<array[i].length; j++){
-            var cloud=array[6][7];
-            cloud.addClass("cloud");
-        }
+//change selected div classes (from the matrix) to create the tree and leaves:
+
+function createTree() {
+    for (var i = 410; i < 415; i++) {
+        var treeLeaves = $("div.tile:nth-child(" + i + ")");
+        treeLeaves.addClass("tileLeaves");
     }
+    for (var i = 440; i < 445; i++) {
+        var treeLeaves = $("div.tile:nth-child(" + i + ")");
+        treeLeaves.addClass("tileLeaves");
+    }
+    for (var i = 470; i < 475; i++) {
+        var treeLeaves = $("div.tile:nth-child(" + i + ")");
+        treeLeaves.addClass("tileLeaves");
+    }
+    for (var i = 500; i < 505; i++) {
+        var treeLeaves = $("div.tile:nth-child(" + i + ")");
+        treeLeaves.addClass("tileLeaves");
+    }
+    var treeLeaves = $("div.tile:nth-child(575)");
+    treeLeaves.addClass("tileLeaves");
+    for (var i = 604; i < 607; i++) {
+        var treeLeaves = $("div.tile:nth-child(" + i + ")");
+        treeLeaves.addClass("tileLeaves");
+    }
+    var treeTrunk = $("div.tile:nth-child(532)");
+    treeTrunk.addClass("tileWood");
+    var treeTrunk2 = $("div.tile:nth-child(562)");
+    treeTrunk2.addClass("tileWood");
+    var treeTrunk3 = $("div.tile:nth-child(592)");
+    treeTrunk3.addClass("tileWood");
+    var treeTrunk4 = $("div.tile:nth-child(622)");
+    treeTrunk4.addClass("tileWood");
 }
 
-createCloud(matrixBackground);
-*/
+createTree();
+
 //function to create top ground tiles in html according to array:
 
 function createTopGroundTiles(array) {
