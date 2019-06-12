2daad08e1ccaeb9eabb52de8ab2116c8755c84a8
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-12 15:19:08 +0200

    cloud added

--- js/minecraft.js
+++ js/minecraft.js
@@ -23,21 +23,21 @@ class Array {
 
 //matrix for sky:
 
-var array1 = new Array(20);
+var array1 = new Array(30);
 var array2 = new Array(20);
 
 var matrixBackground = array1.createMatrix(array2);
 
 //matrix for ground:
 
-var array3 = new Array(20);
-var array4 = new Array(10);
+var array3 = new Array(10);
+var array4 = new Array(20);
 
 var matrixSoil = array3.createMatrix(array4);
 
 //array for top of ground:
 
-var array5 = new Array(20);
+var array5 = new Array(30);
 
 var arraySoilTop = array5.createArray();
 
@@ -58,6 +58,43 @@ function createTiles(array) {
 
 createTiles(matrixBackground);
 
+function createCloud() {
+    for (var i = 217; i < 219; i++) {
+        var cloud = $("div.tile:nth-child(" + i + "");
+        cloud.addClass("cloud");
+    }
+    for (var i = 245; i < 250; i++) {
+        var cloud = $("div.tile:nth-child(" + i + "");
+        cloud.addClass("cloud");
+    }
+    for (var i = 252; i < 255; i++) {
+        var cloud = $("div.tile:nth-child(" + i + "");
+        cloud.addClass("cloud");
+    }
+    for (var i = 274; i < 285; i++) {
+        var cloud = $("div.tile:nth-child(" + i + "");
+        cloud.addClass("cloud");
+    }
+    for (var i = 309; i < 312; i++) {
+        var cloud = $("div.tile:nth-child(" + i + "");
+        cloud.addClass("cloud");
+    }
+}
+
+createCloud();
+
+/*
+function createCloud(array){
+    for (var i=0; i<array.length; i++){
+        for (var j=0; j<array[i].length; j++){
+            var cloud=array[6][7];
+            cloud.addClass("cloud");
+        }
+    }
+}
+
+createCloud(matrixBackground);
+*/
 //function to create top ground tiles in html according to array:
 
 function createTopGroundTiles(array) {
