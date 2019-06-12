3174caed4cd7112ea98f238b9c028d2d6cf98811
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-06-12 12:03:06 +0200

    matrix added, able to change each square on click

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,5 +1,47 @@
-for (var i = 0; i < 30; i++) {
-    var tile = $("<span />");
-    tile.addClass("tile");
-    $("body").append(tile);
+class Array {
+    constructor(length) {
+        this.length = length;
+    }
+    createArray() {
+        var array1 = [];
+        for (var i = 0; i < this.length; i++) {
+            array1.push(i);
+        }
+        return array1;
+    }
+    createMatrix(array) {
+        var outerArray = this.createArray();
+        var innerArray = array.createArray();
+        for (var i = 0; i < this.length; i++) {
+            outerArray.splice(i, 1, innerArray);
+        }
+        return outerArray;
+    }
 }
+
+var array1 = new Array(30);
+var array2 = new Array(30);
+var matrix = array1.createMatrix(array2);
+
+function createTiles(array) {
+    for (var i = 0; i < array.length; i++) {
+        var tileRow = $("<div />");
+        tileRow.addClass("tile");
+        $(".container").append(tileRow);
+        for (var j = 0; j < array[i].length; j++) {
+            var tileCol = $("<div />");
+            tileCol.addClass("tile");
+            $(".container").append(tileCol);
+        }
+    }
+}
+
+createTiles(matrix);
+
+function changeColor() {
+    $(event.target).addClass("tilecolor");
+}
+
+$("div.tile").on("click", changeColor);
+
+
